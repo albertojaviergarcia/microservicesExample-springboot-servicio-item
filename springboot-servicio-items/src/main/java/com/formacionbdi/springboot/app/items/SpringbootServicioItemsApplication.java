3a70c3347509d54bcd4a409debea4e9155853020
Alberto <info@albertojaviergarcia.com>
2020-09-03 11:10:26 +0200

    Added Hystrix and compatibility with Eureka. Setted connection of microservice with Eureka server and disabled Ribbon.

--- springboot-servicio-items/src/main/java/com/formacionbdi/springboot/app/items/SpringbootServicioItemsApplication.java
+++ springboot-servicio-items/src/main/java/com/formacionbdi/springboot/app/items/SpringbootServicioItemsApplication.java
@@ -2,16 +2,22 @@ package com.formacionbdi.springboot.app.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //Feign: Llamadas REST de forma declarativa.
 @EnableFeignClients
+@EnableEurekaClient
 
-//Ribbon: Balanceador de carga entre servicios.
+//Ribbon: Balanceador de carga entre servicios. No necesario con eureka
 //RibbonClient: Indica a Feign los microservicios con los que conectará
-@RibbonClient(name = "servicio-Productos")
+//@RibbonClient(name = "servicio-Productos")
 
+
+//Hystrix: Habilita el control de gestión de errores
+@EnableCircuitBreaker
 
 @SpringBootApplication
 public class SpringbootServicioItemsApplication {
--- springboot-servicio-items/src/main/java/com/formacionbdi/springboot/app/items/controller/ItemController.java
+++ springboot-servicio-items/src/main/java/com/formacionbdi/springboot/app/items/controller/ItemController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.items.models.entity.Item;
+import com.formacionbdi.springboot.app.items.models.entity.Producto;
 import com.formacionbdi.springboot.app.items.models.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -23,9 +25,21 @@ public class ItemController {
 		return serviceItem.findAll();
 	}
 	
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return serviceItem.findById(id, cantidad);
+	}
+	
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto p = new Producto();
+		p.setNombre("Producto alternativo");
+		p.setPrecio(134.56);
+		p.setId((long) -99);
+		item.setProducto(p);
+		item.setCantidad(0);
+		return item;
 	}
 	
 }
